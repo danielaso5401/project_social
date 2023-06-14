d57610e27ad58d61aa525dee3541f8b2f5077c84
Daniel Huaita Carpio <42683702+danielaso5401@users.noreply.github.com>
2023-06-14 12:10:48 -0500

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './layout/public/login/login/login.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'IniciarSesion', component: LoginComponent },
+  { path: '**', component: AppComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
